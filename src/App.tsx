877110f499d513c36ef045b7e51b637948a9ecfe
joel-furrer <joel.furrer426@gmail.com>
2024-12-01 16:57:11 +0100

    Changed Style Options to App.css | Added Containers to App.tsx | Change Style Options to index.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import './App.css'
+import './App.css';
 
 function App() {
-  
-
   return (
-    <p>test</p>
-  )
+    <div className="main">
+      <header className="header">
+        <h1>Food Inspo</h1>
+      </header>
+
+      <div className="contentBox">
+        <p>Willkommen zurück, Eric</p>
+      </div>
+
+      <footer className="footer">
+        <p>Impressum: Eric Lücking © 2024</p>
+      </footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
