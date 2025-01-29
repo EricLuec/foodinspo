e29a1663a8025be2d02054d821c6745394d17054
ericluec <eric.luecking@gmail.com>
2025-01-29 17:51:05 +0100

    chakra ui added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,13 @@
+import { Button, HStack } from '@chakra-ui/react';
 import './App.css';
 
 function App() {
   return (
-    <div className="main">
-      <header className="header">
-        <h1>Food Inspo</h1>
-      </header>
-
-      <div className="contentBox">
-        <p>Willkommen zurück, Eric</p>
-      </div>
-
-      <footer className="footer">
-        <p>Impressum: Eric Lücking © 2024</p>
-      </footer>
-    </div>
+    <HStack>
+      <Button>
+        hello
+      </Button>
+    </HStack>
   );
 }
 
